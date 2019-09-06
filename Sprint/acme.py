4415381dc04aac6389ca139d84e1d7755ade7340
Jaydenzk <52672607+Jaydenzk@users.noreply.github.com>
2019-09-06 11:32:21 -0700

    Update acme.py

--- Sprint/acme.py
+++ Sprint/acme.py
@@ -32,7 +32,10 @@ def stealability(self):
         x = (self.price)/(self.weight)
 
         """
-        Determination of stealaility is given price/a_weight
+        Determination of stealaility is given price/a_weight 
+        when x<0.5 print "Not so stealable"
+        and 0.5<x<1 print "Kinda stealable"
+        otherwise print "Very stealable"
         """
 
         if x < 0.5:
@@ -47,6 +50,9 @@ def explode(self):
 
     """
     Determination of explode is given flammability/weight
+    when y<10 print "...fizzle"
+    and 10<y<50 print "...boom"
+    otherwise print "...BABOOM"
     """
 
     y = (self.flammability)*(self.weight)
@@ -67,7 +73,7 @@ Subclass BoxingGlove
 class BoxingGlove(Product):
 
     """
-    Class to show a boxing BoxingGlove and default weight change to 10
+    Class to model BoxingGlove and default weight change to 10
     """
 
     random = random.randint(1000000, 9999999)
@@ -83,7 +89,7 @@ def __init__(self, name, price=10, weight=10, flammability=0.5,
 
 
 def explode(self):
-    """If boxing glove override, explode statement always it's a glove"""
+    """If boxing glove override, explode statement always print "it's a glove" """
     print("...it's a glove")
 
 
